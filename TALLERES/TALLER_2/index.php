08dc621884ea3fec10a7f35adf02c17b03471f51
Davis <112843524+DviSantos@users.noreply.github.com>
2024-08-22 22:53:31 -0500

    Ejercicio práctico: combinación de conceptos aprendidos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,50 +1,28 @@
 <?php
-//Paso 3: Definición y asignación de variables
-$nombre = "Juan";
-$edad = 25;
-$altura = 1.75;
-$esEstudiante = true;
+// Definición de variables
+$nombre = "María";
+$edad = 30;
+$ciudad = "Madrid";
 
-echo "Nombre: $nombre<br>";
-echo "Edad: $edad<br>";
-echo "Altura: $altura<br>";
-echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
+// Definición de constante
+define("PROFESION", "Ingeniera");
 
-//Paso 4: Concatenación de variables y constantes
-$nombre = "Juan";
-$edad = 25;
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Vivo en $ciudad y soy " . PROFESION . ".";
 
-// Concatenación usando el operador .
-$presentacion1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
 
-// Concatenación dentro de comillas dobles
-$presentacion2 = "Hola, mi nombre es $nombre y tengo $edad años.";
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
 
-// Definición de una constante
-define("SALUDO", "¡Bienvenido!");
-
-// Concatenación con constante
-$mensaje = SALUDO . " " . $nombre;
-
-echo $presentacion1 . "<br>";
-echo $presentacion2 . "<br>";
-echo $mensaje . "<br>";
-
-//Paso 5: Impresión de texto en PHP
-$nombre = "Juan";
-$edad = 25;
-
-// Usando echo
-echo "Hola, mundo!<br>";
-echo "Mi nombre es $nombre<br>";
-
-// Usando print
-print "Tengo $edad años<br>";
-
-// Usando printf (permite formateo)
-printf("Me llamo %s y tengo %d años<br>", $nombre, $edad);
-
-// Usando var_dump (útil para debugging)
+echo "<br>Información de debugging:<br>";
 var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($ciudad);
+echo "<br>";
+var_dump(PROFESION);
 echo "<br>";
 ?>
